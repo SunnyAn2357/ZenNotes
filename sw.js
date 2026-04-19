a177a9915500de9d0e3198b04270ceb491f322d6
SunnyAn2357 <96528964+SunnyAn2357@users.noreply.github.com>
2026-04-19 22:41:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 캐시 이름 (앱을 업데이트할 때마다 v2, v3... 로 숫자를 올려주세요!)
-const CACHE_NAME = 'zennotes-cache-v3.1.1';
+const CACHE_NAME = 'zennotes-cache-v3.1.2';
 
 // 반드시 캐싱해야 할 내 서버의 기본 파일들
 const STATIC_ASSETS = [
